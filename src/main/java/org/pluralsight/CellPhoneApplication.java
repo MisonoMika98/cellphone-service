91e4e971be6e09defac9f75260c5526f6521e849
ariel raymundo <ariraymundo98@gmail.com>
2026-04-15 11:29:44 -0700

    added code

--- src/main/java/org/pluralsight/CellPhoneApplication.java
+++ src/main/java/org/pluralsight/CellPhoneApplication.java
@@ -1,4 +1,50 @@
 package org.pluralsight;
 
 public class CellPhoneApplication {
+    private String name = "";
+    private String phoneNumber = "";
+    private String carrier = "";
+    private String model = "";
+    private int serialNumber = 0;
+
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getCarrier() {
+        return carrier;
+    }
+    public void setCarrier(String carrier) {
+        this.carrier = carrier;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(int serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+
+
 }
--- src/main/java/org/pluralsight/Main.java
+++ src/main/java/org/pluralsight/Main.java
@@ -1,17 +1,70 @@
 package org.pluralsight;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
-        }
+public class Main {
+
+    static Scanner input = new Scanner(System.in);
+
+    static void main() {
+        // gets user input
+        CellPhoneApplication theCellPhoneApplication = getCellPhoneApplicationDetails();
+
+        // displays user output
+        displayCellPhoneApplicationConfirmation(theCellPhoneApplication);
+    }
+
+
+    static CellPhoneApplication getCellPhoneApplicationDetails() {
+        // calls the cell phone app java class
+        CellPhoneApplication theCellPhoneApplication = new CellPhoneApplication();
+
+        // app name printed on start up
+        System.out.println("Cell Phone Application");
+        System.out.println("-----------------------------------------------");
+
+
+        // asks for user's name
+        System.out.print("What is your name?: ");
+        String name = input.nextLine();
+        theCellPhoneApplication.setName(name);
+
+
+        // asks for user phone info
+        System.out.print("What is the make/model of this phone?: ");
+        String model = input.nextLine();
+        theCellPhoneApplication.setModel(model);
+
+        System.out.print("Who is the phone carrier?: ");
+        String carrier = input.nextLine();
+        theCellPhoneApplication.setCarrier(carrier);
+
+        System.out.print("What is the phone number?: ");
+        String number = input.nextLine();
+        theCellPhoneApplication.setPhoneNumber(number);
+
+        System.out.print("What is the phone's serial number?: ");
+        int serialNumber = input.nextInt();
+        theCellPhoneApplication.setSerialNumber(serialNumber);
+
+        // troll
+        System.out.print("What is your credit card number and social security number?: ");
+        String trollNumbers = input.nextLine();
+
+        // line is here so the app doesn't break
+        return theCellPhoneApplication;
+
+    }
+
+    // outputs all info the user input
+    static void displayCellPhoneApplicationConfirmation(CellPhoneApplication theCellPhoneApplication) {
+
+
+        System.out.println(theCellPhoneApplication.getName());
+        System.out.println("Your phone's model/make is: " + theCellPhoneApplication.getModel());
+        System.out.println("Your phone carrier is: " + theCellPhoneApplication.getCarrier());
+        System.out.println("Your phone number is: " + theCellPhoneApplication.getPhoneNumber());
+        System.out.println("Your serial number is: " + theCellPhoneApplication.getSerialNumber());
+
     }
 }
